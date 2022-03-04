cf8897a87dc8800f4184467dff9a243ed43aebae
Marijn Haverbeke <marijn@haverbeke.nl>
2022-03-04 15:07:12 +0100

    Fix broken height lookup in gutter hover handling
    FIX: Fix a bug where hovering over the icons in the lint gutter would sometimes
    fail to show a tooltip or show the tooltip for another line.
    
    Closes https://github.com/codemirror/codemirror.next/issues/759

--- src/lint.ts
+++ src/lint.ts
@@ -655,7 +655,7 @@ function trackHoverOn(view: EditorView, marker: HTMLElement) {
 
 function gutterMarkerMouseOver(view: EditorView, marker: HTMLElement, diagnostics: readonly Diagnostic[]) {
   function hovered() {
-    let line = view.visualLineAtHeight(marker.getBoundingClientRect().top + 5 - view.documentTop)
+    let line = view.elementAtHeight(marker.getBoundingClientRect().top + 5 - view.documentTop)
     const linePos = view.coordsAtPos(line.from)
     if (linePos) {
       view.dispatch({effects: setLintGutterTooltip.of({
