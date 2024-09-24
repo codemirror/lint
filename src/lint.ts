35644c10cf4076e8cce8f902eb695a0315429e58
Marijn Haverbeke <marijn@haverbeke.berlin>
2024-09-24 10:09:39 +0200

    Don't wait for the slowest linter before showing results
    FIX: When multiple linters are installed, start displaying results from ones that return quickly
    even if others are slow to return.
    
    See https://discuss.codemirror.net/t/mixing-async-linter-with-immediate-linter/8654

--- src/lint.ts
+++ src/lint.ts
@@ -304,14 +304,10 @@ const lintPlugin = ViewPlugin.fromClass(class {
     } else {
       this.set = false
       let {state} = this.view, {sources} = state.facet(lintConfig)
-      if (sources.length) Promise.all(sources.map(source => Promise.resolve(source(this.view)))).then(
-        annotations => {
-          let all = annotations.reduce((a, b) => a.concat(b))
-          if (this.view.state.doc == state.doc)
-            this.view.dispatch(setDiagnostics(this.view.state, all))
-        },
-        error => { logException(this.view.state, error) }
-      )
+      if (sources.length) batchResults(sources.map(s => Promise.resolve(s(this.view))), annotations => {
+        if (this.view.state.doc == state.doc)
+          this.view.dispatch(setDiagnostics(this.view.state, annotations.reduce((a, b) => a.concat(b))))
+      }, error => { logException(this.view.state, error) })
     }
   }
 
@@ -338,6 +334,16 @@ const lintPlugin = ViewPlugin.fromClass(class {
     clearTimeout(this.timeout)
   }
 })
+
+function batchResults<T>(promises: readonly Promise<T>[], sink: (values: T[]) => void, error: (reason: any) => void) {
+  let collected: T[] = [], timeout = -1
+  for (let p of promises) p.then(value => {
+    collected.push(value)
+    clearTimeout(timeout)
+    if (collected.length == promises.length) sink(collected)
+    else setTimeout(() => sink(collected), 200)
+  }, error)
+}
 
 const lintConfig = Facet.define<{source: LintSource | null, config: LintConfig},
                                 Required<LintConfig> & {sources: readonly LintSource[]}>({
